c905108529a5c4db4ea6d48b52a46eb6f5353234
Willy <willy548798@gmail.com>
2026-01-25 23:35:09 +0800

    feat: 優化 IndexViewController

--- CustomTabBar/Controller/IndexController.swift
+++ CustomTabBar/Controller/IndexController.swift
@@ -21,23 +21,51 @@ final class IndexController: UIViewController {
     
     func showMainApplication() {
         let tabBarController = TabBarConfiguration.makeTabBarController()
-        transitionToViewController(tabBarController)
+        transitionToViewController(tabBarController, animated: false)
     }
     
-    private func transitionToViewController(_ viewController: UIViewController) {
-        if let currentVC = currentChildViewController {
-            currentVC.willMove(toParent: nil)
-            currentVC.view.removeFromSuperview()
-            currentVC.removeFromParent()
+    private func transitionToViewController(_ viewController: UIViewController, animated: Bool = true) {
+        guard viewController !== currentChildViewController else { return }
+        
+        let previousVC = currentChildViewController
+        
+        if let previousVC = previousVC {
+            previousVC.willMove(toParent: nil)
+            
+            if animated {
+                UIView.transition(
+                    with: view,
+                    duration: 0.3,
+                    options: [.transitionCrossDissolve, .allowUserInteraction],
+                    animations: {
+                        previousVC.view.alpha = 0
+                    },
+                    completion: { _ in
+                        previousVC.view.removeFromSuperview()
+                        previousVC.removeFromParent()
+                    }
+                )
+            } else {
+                previousVC.view.removeFromSuperview()
+                previousVC.removeFromParent()
+            }
         }
         
         addChild(viewController)
         view.addSubview(viewController.view)
-        viewController.view.snp.makeConstraints { make in
+        
+        viewController.view.snp.remakeConstraints { make in
             make.edges.equalToSuperview()
         }
-        viewController.didMove(toParent: self)
         
+        if animated && previousVC != nil {
+            viewController.view.alpha = 0
+            UIView.animate(withDuration: 0.3, delay: 0, options: [.allowUserInteraction]) {
+                viewController.view.alpha = 1
+            }
+        }
+        
+        viewController.didMove(toParent: self)
         currentChildViewController = viewController
     }
 }
