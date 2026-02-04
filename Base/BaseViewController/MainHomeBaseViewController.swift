31da61e53c359362e9fd220917156538cfd7e96b
Willy <willy548798@gmail.com>
2026-02-04 23:32:49 +0800

    feat: 新增 BaseViewController

---
+++ Base/BaseViewController/MainHomeBaseViewController.swift
@@ -0,0 +1,61 @@
+//
+//  MainHomeBaseViewController.swift
+//  TheSportSP
+//
+//  Created by Willy Hsu on 2026/2/4.
+//
+
+import UIKit
+import SnapKit
+
+@MainActor
+class MainHomeBaseViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let layout: UICollectionViewLayout
+    
+    // MARK: - UI Components
+    
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        collectionView.alwaysBounceVertical = true
+        collectionView.showsVerticalScrollIndicator = true
+        return collectionView
+    }()
+    
+    // MARK: - Initialization
+    
+    init(layout: UICollectionViewLayout = UICollectionViewFlowLayout()) {
+        if let flowLayout = layout as? UICollectionViewFlowLayout {
+            flowLayout.minimumLineSpacing = 8
+            flowLayout.minimumInteritemSpacing = 0
+        }
+        self.layout = layout
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    // MARK: - Setup
+    
+    private func setupUI() {
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(collectionView)
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
